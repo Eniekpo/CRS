7b51e6a17b32f6a02a540c90560ce55159817328
Eniekpo <johneniekpo4life@gmail.com>
2023-04-14 15:22:18 +0100

    a

--- App/models.py
+++ App/models.py
@@ -4,13 +4,31 @@ from sklearn.ensemble import RandomForestClassifier
 import joblib
 
 # Create your models here.
+DEPARTMENT = (
+    ('', 'Department'),
+    ('ICT', 'ICT'),
+    ('ICT', 'ICT'),
+    ('ICT', 'ICT'),
+    ('ICT', 'ICT'),
+)
+
+MINISTRY = (
+    ('', 'Ministry'),
+    ('Agriculture', 'Agriculture'),
+    ('Agriculture', 'Agriculture'),
+    ('Agriculture', 'Agriculture'),
+    ('Agriculture', 'Agriculture'),
+    ('Agriculture', 'Agriculture'),
+)
+
+
 
 class Civilservants(models.Model):
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
     gender = models.CharField(max_length=50)
-    ministry = models.CharField(max_length=100)
-    department = models.CharField(max_length=50)
+    ministry = models.CharField(max_length=100, choices=MINISTRY)
+    department = models.CharField(max_length=50, choices=DEPARTMENT)
     marital_status = models.CharField(max_length=50)
     date_of_birth = models.DateField()
     phone = models.CharField(max_length=50)
@@ -28,11 +46,11 @@ class Civilservants(models.Model):
 
 class Retirement(models.Model):
     retiree_name =  (models.ForeignKey(Civilservants, on_delete=models.CASCADE))
-    retiree_ministry = models.CharField(max_length=100)
-    retiree_department = models.CharField(max_length=100)
+    retiree_ministry = models.CharField(max_length=100, choices=MINISTRY)
+    retiree_department = models.CharField(max_length=100, choices=DEPARTMENT)
     appointment_date = models.DateField()
     dob = models.DateField()
-    age = models.IntegerField()
+    age = models.IntegerField(validators=[MinValueValidator(18), MaxValueValidator(75)], null=True)
     years_of_work = models.IntegerField()
     retirement_status = models.CharField(max_length=50, blank=True)
 
@@ -52,8 +70,8 @@ class Retirement(models.Model):
         verbose_name_plural = 'retirement'
 class Reports(models.Model):
     Retname =  (models.ForeignKey(Civilservants, on_delete=models.CASCADE))
-    Minist = models.CharField(max_length=100)
-    Dept = models.CharField(max_length=100)
+    Minist = models.CharField(max_length=100, choices=MINISTRY)
+    Dept = models.CharField(max_length=100, choices=DEPARTMENT)
     Apt_date = models.DateField()
     Dob = models.DateField()
     Age = models.IntegerField()
